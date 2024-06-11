5afafd739d1c5f0969be6ca207409576f2e77cb3
nghugo <nghugoryan@gmail.com>
2024-06-11 23:45:20 +0100

    add styling shortcut

---
+++ app/buttonPage.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import { Button } from "react-native-paper";
+
+// import { name as appName } from "./app.json";
+// import App from "./src/App";
+
+const ButtonPage = () => {
+  return (<Button icon="camera">Original Style Button</Button>);
+};
+
+export default ButtonPage;
--- app/index.js
+++ app/index.js
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from "react-native-paper";
+import { Link } from "expo-router";
+import { customLightColors } from "@theme/colors";
+import { Button } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: customLightColors.colors,
+};
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home, Hello!</Text>
-        </View>
-    )
-}
+  return (
+    <PaperProvider theme={theme}>
+      <View>
+        <Text>Home, Hello!</Text>
+        <Link href="/buttonPage">ButtonPage</Link>
+        <Button icon="camera">Custom Style Button</Button>
+      </View>
+    </PaperProvider>
+  );
+};
 
-export default Home
+export default Home;

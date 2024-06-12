e940d0f99d6495d301822773c814aa3f163d742b
nghugo <nghugoryan@gmail.com>
2024-06-12 19:09:50 +0100

    add dark mode

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,12 @@
 import { Stack } from "expo-router";
 import React from "react";
 
+import { useColorScheme, View, StyleSheet, Text } from "react-native";
 import {
   MD3LightTheme,
   MD3DarkTheme,
   PaperProvider,
 } from "react-native-paper";
-import { useColorScheme } from "react-native";
 
 import { customLightColors } from "../theme/colors";
 import { customDarkColors } from "../theme/colors";
@@ -15,15 +15,32 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Layout() {
     const colorScheme = useColorScheme();
 
+
     const paperTheme =
         colorScheme === 'dark'
-        // true
         ? { ...MD3DarkTheme, colors: customDarkColors.colors }
         : { ...MD3LightTheme, colors: customLightColors.colors };
     
     return (
         <PaperProvider theme={paperTheme}>
-            <Stack />
+            <SafeAreaView style={[styles.safeArea, { 
+                backgroundColor: paperTheme.colors.background
+                 }]}>
+                <View style={{ flex: 1, backgroundColor: paperTheme.colors.background }}>
+                    
+                    {/* The stack -> always light mode for some reason */}
+                    <Stack />
+                </View>
+            </SafeAreaView>
         </PaperProvider>
     );
 }
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+    },
+});

6b499ab61d82589ad3ced8bce4674ba598648a2e
nghugo <nghugoryan@gmail.com>
2024-06-15 00:37:52 +0100

    add styled login page

--- src/app/auth/login.js
+++ src/app/auth/login.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useState } from "react";
-import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
+import { Text, View, Image, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 import { Link } from "expo-router";
 import { fontSize } from "src/styles/fontConfig";
@@ -27,16 +27,12 @@ export default function Another() {
           justifyContent: "space-between",
           height: "100%",
           flexGrow: 1,
-          borderColor: "red",
-          borderWidth: 1,
         }}
       >
         <View
           style={{
             justifyContent: "space-between",
-            borderColor: "green",
-            borderWidth: 1,
-            height: "60%",
+            height: "55%",
           }}
         >
           <View>
@@ -77,58 +73,46 @@ export default function Another() {
               height: "50%",
               justifyContent: "flex-end",
               gap: 6,
-              borderColor: "blue",
-              borderWidth: 1,
             }}
           >
             <TextInput
               label="Email"
               // value={text}
               mode="outlined"
-              onChangeText={(text) => setText(text)}
-            />
-            <TextInput
-              label="Email"
-              // value={text2}
-              mode="outlined"
-              onChangeText={(text) => setText(text)}
+              // onChangeText={(text) => setText(text)}              }
             />
             <TextInput
               label="Password"
+              // value={text2}
+              mode="outlined"
+              // onChangeText={(text) => setText(text)}
+            />
+            <TextInput
+              label="Confirm Password"
               // value={text3}
               mode="outlined"
-              onChangeText={(text) => setText(text)}
+              // onChangeText={(text) => setText(text)}
             />
           </View>
         </View>
         <View
           style={{
-            borderColor: "grey",
-            borderWidth: 1,
             // height: 100,
             gap: 10,
             marginBottom: 60,
             justifyContent: "space-between",
           }}
         >
-          <Button
-            mode="contained"
-            onPress={handleSubmit}
-            style={{ height: 50, borderRadius: 25, justifyContent: "center" }}
-          >
-            <Text
-            style={{
-              fontWeight: "600",
-              fontSize: fontSize.LARGE}}
-            >Log In</Text>
-          </Button>
 
-          {/* <TouchableOpacity onPress={handleSubmit} style={styles.touchable}>
-            <Button mode="contained" style={styles.button}>
-              <Text style={styles.buttonText}>Log In</Text>
+          <TouchableOpacity onPress={handleSubmit}>
+            <Button mode="contained" style={{ height: 50, borderRadius: 25, justifyContent: "center" }}>
+              <Text style={{
+              fontWeight: "600",
+              fontSize: fontSize.LARGE}}>Log In</Text>
             </Button>
-          </TouchableOpacity> */}
-          
+          </TouchableOpacity>
+
+
           <Text
             style={{
               fontSize: fontSize.MEDIUM,
@@ -155,6 +139,7 @@ export default function Another() {
 const styles = StyleSheet.create({
   nhsLogoContainer: {
     alignItems: "center",
+    paddingTop: 32,
     padding: 16,
   },
   nhsLogo: {

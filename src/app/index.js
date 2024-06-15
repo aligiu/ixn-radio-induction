a00a71f240c29c4fc52d3fed8e06119a6a3cbdf2
nghugo <nghugoryan@gmail.com>
2024-06-15 23:41:55 +0100

    add basic navblock

--- src/app/index.js
+++ src/app/index.js
@@ -16,12 +16,48 @@ export default function Home() {
         {/* Scroll view needed to dismiss search bar */}
 
         <TText style={styles.pageTitle}>Home</TText>
+
         <View>
-          <Image
-            resizeMethod="contain"
-            source={require("assets/images/nhs-logo-square.png")}
-            style={styles.nhsSquare}
-          />
+          {/* NavBlock */}
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 10,
+              borderWidth: 1,
+              borderColor: "grey",
+              borderRadius: 20,
+              padding: 10,
+            }}
+          >
+            <View>
+              <Image
+                resizeMethod="contain"
+                source={require("assets/images/nhs-logo-square.png")}
+                style={styles.nhsSquare}
+              />
+            </View>
+
+            <View style={{ flex: 1 }}>
+              <TText
+                style={{
+                  fontSize: fontSize.MEDIUM,
+                  fontFamily: "InterMedium",
+                }}
+              >
+                Ashford and St Peter's
+              </TText>
+              <TText
+              style={{
+                fontSize: fontSize.SMALL,
+                fontFamily: "InterRegular",
+              }}
+              >
+                {/* find way to abridge long text */}
+                Parking, Induction, Study Leave, PCAS and Logins, ...
+              </TText>
+            </View>
+          </View>
+          {/* NavBlock End */}
         </View>
         <Link href="/another">
           <TText>Go to another page{"\n"}</TText>

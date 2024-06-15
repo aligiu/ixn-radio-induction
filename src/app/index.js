08c14cfacb4d4e9e18185391ea2c487610361bec
nghugo <nghugoryan@gmail.com>
2024-06-15 23:50:23 +0100

    extract navblock

--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,7 @@ import { contentContainerStyles } from "/src/styles/contentContainer";
 
 import { TText } from "./_layout";
 import { fontSize } from "src/styles/fontConfig";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Home() {
   return (
@@ -17,51 +18,25 @@ export default function Home() {
 
         <TText style={styles.pageTitle}>Home</TText>
 
-        <View>
-          {/* NavBlock */}
-          <View
-            style={{
-              flexDirection: "row",
-              gap: 10,
-              borderWidth: 1,
-              borderColor: "grey",
-              borderRadius: 20,
-              padding: 10,
-            }}
-          >
-            <View>
-              <Image
-                resizeMethod="contain"
-                source={require("assets/images/nhs-logo-square.png")}
-                style={styles.nhsSquare}
-              />
-            </View>
+        <View style={{gap: 10}}>
 
-            <View style={{ flex: 1 }}>
-              <TText
-                style={{
-                  fontSize: fontSize.MEDIUM,
-                  fontFamily: "InterMedium",
-                }}
-              >
-                Ashford and St Peter's
-              </TText>
-              <TText
-              style={{
-                fontSize: fontSize.SMALL,
-                fontFamily: "InterRegular",
-              }}
-              >
-                {/* find way to abridge long text */}
-                Parking, Induction, Study Leave, PCAS and Logins, ...
-              </TText>
-            </View>
-          </View>
-          {/* NavBlock End */}
+          <NavBlock
+            imageSource={require("assets/images/nhs-logo-square.png")}
+            title={"Ashford and St Peter's"}
+            description={
+              "Parking, Induction, Study Leave, PCAS and Logins, ..."
+            }
+          />
+
+          <NavBlock
+            imageSource={require("assets/images/nhs-logo-square.png")}
+            title={"Ashford and St Peter's"}
+            description={
+              "Parking, Induction, Study Leave, PCAS and Logins, ..."
+            }
+          />
         </View>
-        <Link href="/another">
-          <TText>Go to another page{"\n"}</TText>
-        </Link>
+
       </ScrollView>
     </>
   );
@@ -80,3 +55,54 @@ const styles = StyleSheet.create({
     borderRadius: 15,
   },
 });
+
+// require("assets/images/nhs-logo-square.png")
+// "Ashford and St Peter's"
+// "Parking, Induction, Study Leave, PCAS and Logins, ..."
+
+// find way to abridge long text
+
+function NavBlock({ title, description, imageSource }) {
+  return (
+    <TouchableOpacity>
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 10,
+          borderWidth: 1,
+          borderColor: "grey",
+          borderRadius: 20,
+          padding: 10,
+        }}
+      >
+        <View>
+          <Image
+            resizeMethod="contain"
+            source={imageSource}
+            style={styles.nhsSquare}
+          />
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <TText
+            style={{
+              fontSize: fontSize.MEDIUM,
+              fontFamily: "InterMedium",
+            }}
+          >
+            {title}
+          </TText>
+          <TText
+            style={{
+              fontSize: fontSize.SMALL,
+              fontFamily: "InterRegular",
+            }}
+          >
+            {/* find way to abridge long text */}
+            {description}
+          </TText>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}

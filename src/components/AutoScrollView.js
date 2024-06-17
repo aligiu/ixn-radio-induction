aee92305c91554b1a46770b3ed4e7d8abb090454
nghugo <nghugoryan@gmail.com>
2024-06-17 21:07:59 +0100

    bug fix clickable autocomplete

--- src/components/AutoScrollView.js
+++ src/components/AutoScrollView.js
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
 
-import { View, ScrollView } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 
 import { useTheme } from "react-native-paper";
 
 import { contentContainerStyles } from "src/styles/contentContainer";
 
-import { fontSize } from "../styles/fontConfig";
 import SearchAutocompleteElement from "../components/searchAutocompleteElement";
 import SearchbarContext from "../context/SearchbarContext";
 
@@ -15,48 +14,49 @@ export default function AutoScrollView({ children }) {
   const { searchbarInFocus, setSearchbarInFocus } =
     useContext(SearchbarContext);
 
+  // If search bar is not in focus, then display children normally
+  // Otherwise, display the search bar results
+  if (!searchbarInFocus) {
+    return (
+      <ScrollView
+        keyboardDismissMode="on-drag"
+        contentContainerStyle={{ flexGrow: 1 }}
+        style={contentContainerStyles.container}
+      >
+        {children}
+      </ScrollView>
+    );
+  }
   return (
     <ScrollView
-      keyboardDismissMode="on-drag"
-      contentContainerStyle={{ flexGrow: 1 }}
-      style={contentContainerStyles.container}
+      style={
+        contentContainerStyles.container
+        // {backgroundColor: theme.colors.background}
+      }
+      keyboardShouldPersistTaps="always"
     >
-      {/* 
-        If search bar is not in focus, then display children normally
-        Otherwise, display the search bar results
-        */}
-      {!searchbarInFocus && children}
-      {searchbarInFocus && (
-        <ScrollView
-          style={{
-            backgroundColor: theme.colors.background,
-          }}
-          keyboardShouldPersistTaps="always"
-        >
-          <View
-            style={{
-              flexDirection: "column",
-              gap: 10, // gap must be placed in <View> not <ScrollView>
-            }}
-          >
-            <SearchAutocompleteElement
-              autocompleteText={"Radiopaedia"}
-              topic={"Educational Resources"}
-              section={"Login"}
-              routerLink={"/dummy"}
-              setSearchbarInFocus={setSearchbarInFocus}
-            />
+      <View
+        style={{
+          flexDirection: "column",
+          gap: 10, // gap must be placed in <View> not <ScrollView>
+        }}
+      >
+        <SearchAutocompleteElement
+          autocompleteText={"Radiopaedia"}
+          topic={"Educational Resources"}
+          section={"Login"}
+          routerLink={"/dummy"}
+          setSearchbarInFocus={setSearchbarInFocus}
+        />
 
-            <SearchAutocompleteElement
-              autocompleteText={"Radiopaedia"}
-              topic={"Conferences"}
-              section={"Link"}
-              routerLink={"/dummy"}
-              setSearchbarInFocus={setSearchbarInFocus}
-            />
-          </View>
-        </ScrollView>
-      )}
+        <SearchAutocompleteElement
+          autocompleteText={"Radiopaedia"}
+          topic={"Conferences"}
+          section={"Link"}
+          routerLink={"/dummy"}
+          setSearchbarInFocus={setSearchbarInFocus}
+        />
+      </View>
     </ScrollView>
   );
 }

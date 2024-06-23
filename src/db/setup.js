dd8e7f59b4d53dcc079bcf22cceb3dcf4d0a3738
nghugo <nghugoryan@gmail.com>
2024-06-23 22:06:14 +0100

    reset schema

--- src/db/setup.js
+++ src/db/setup.js
@@ -1,30 +1,14 @@
 import { mockedData } from "./mockedData";
 
-// Users should not be stored locally!!!
-
 export async function setup(db) {
   try {
-    // Create each table if not exists and insert initial data
-    // await db.execAsync(`CREATE TABLE IF NOT EXISTS Users (
-    //                   email TEXT PRIMARY KEY NOT NULL,
-    //                   hashed_password TEXT NOT NULL,
-    //                   is_admin BOOLEAN
-    //   )`);
-    await db.execAsync(`CREATE TABLE IF NOT EXISTS Content (
+    // Delete all existing data
+    await db.execAsync(`DROP TABLE IF EXISTS Content`);
+
+    // Initialize schema
+    await db.execAsync(`CREATE TABLE Content (
             json_string TEXT
     )`);
-
-    // Delete all existing data
-    // await db.execAsync(`DELETE FROM Users`);
-    await db.execAsync(`DELETE FROM Content`);
-
-    // // Insert data for Users
-    // await db.execAsync(`
-    //               INSERT INTO Users 
-    //               VALUES ('user1@example.com', '111', 1),
-    //                      ('user2@example.com', '222', 0),
-    //                      ('user3@example.com', '333', 0);
-    //           `);
 
     // Insert data for Content (prepared statement can take parameters)
     const statement = await db.prepareAsync(
@@ -35,8 +19,24 @@ export async function setup(db) {
 
     // Confirm success
     console.log(`Data has been reset in ${db.databaseName}`);
-
   } catch (error) {
     console.error("Error executing SQL: ", error);
   }
 }
+
+// Users should not be stored locally!!!
+
+// Create each table if not exists and insert initial data
+// await db.execAsync(`CREATE TABLE IF NOT EXISTS Users (
+//                   email TEXT PRIMARY KEY NOT NULL,
+//                   hashed_password TEXT NOT NULL,
+//                   is_admin BOOLEAN
+//   )`);
+
+// // Insert data for Users
+// await db.execAsync(`
+//               INSERT INTO Users
+//               VALUES ('user1@example.com', '111', 1),
+//                      ('user2@example.com', '222', 0),
+//                      ('user3@example.com', '333', 0);
+//           `);

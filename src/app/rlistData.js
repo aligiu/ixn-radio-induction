d909693aefefa430a6b56fee1ede4a31ca5e6d32
nghugo <nghugoryan@gmail.com>
2024-06-27 22:26:43 +0100

    add rearrangable quicklinks

--- src/app/rlistData.js
+++ src/app/rlistData.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
 import DraggableFlatList, {
   ScaleDecorator,
   onDragEnd,
@@ -15,8 +15,11 @@ function getColor(i) {
   const colorVal = i * multiplier;
   return `rgb(${colorVal}, ${Math.abs(128 - colorVal)}, ${255 - colorVal})`;
 }
-
 import { useSQLiteContext } from "expo-sqlite";
+import { useNavigation } from "@react-navigation/native";
+
+import { TText } from "./_layout";
+import { fontSize } from "src/styles/fontConfig";
 
 export default function Rearrangablelist() {
   const screenWidth = Dimensions.get("window").width;
@@ -42,14 +45,11 @@ export default function Rearrangablelist() {
       <ScaleDecorator>
         <View
           style={{
-            borderWidth: 2,
             minWidth: 100,
             minHeight: 100,
             marginTop: 10,
-            borderWidth: 2,
             marginLeft: 10,
             marginRight: 10,
-            borderColor: "blue",
           }}
         >
           <TouchableOpacity
@@ -60,7 +60,16 @@ export default function Rearrangablelist() {
               { backgroundColor: isActive ? "red" : item.backgroundColor },
             ]}
           >
-            <Text>{item.title}</Text>
+            <View style={{ width: "100%" }}>
+              <NavBlock
+                imageSource={require("assets/images/nhs-logo-square.png")}
+                title={item.title}
+                route={`/topics/${item.key}`}
+                description={item.description}
+                content={item.content}
+              />
+              <Text>{item.title}</Text>
+            </View>
           </TouchableOpacity>
         </View>
       </ScaleDecorator>
@@ -115,4 +124,77 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
+  pageTitle: {
+    fontSize: fontSize.LARGE,
+    fontFamily: "InterSemiBold",
+    paddingBottom: 16,
+  },
+  nhsSquare: {
+    height: 60,
+    aspectRatio: 1,
+    borderWidth: 1,
+    borderRadius: 15,
+  },
 });
+
+function NavBlock({ title, description, imageSource, content }) {
+  const navigation = useNavigation();
+
+  const goToTopics = (content) => {
+    navigation.navigate(`topics/[id]`, { content: content, title: title });
+    // pass parameters to pages https://reactnavigation.org/docs/params/#what-should-be-in-params
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        goToTopics(content);
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 10,
+          borderWidth: 1,
+          borderColor: "grey",
+          borderRadius: 20,
+          padding: 10,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            resizeMethod="contain"
+            source={imageSource}
+            style={styles.nhsSquare}
+          />
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <TText
+            style={{
+              fontSize: fontSize.MEDIUM,
+              fontFamily: "InterMedium",
+            }}
+          >
+            {title}
+          </TText>
+          <TText
+            style={{
+              fontSize: fontSize.SMALL,
+              fontFamily: "InterRegular",
+            }}
+            // numberOfLines="2"  // uncomment to abridge text
+            // ellipsizeMode="tail"  // uncomment to abridge text
+          >
+            {description}
+          </TText>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}

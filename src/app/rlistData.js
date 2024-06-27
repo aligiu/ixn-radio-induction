5fd2701833816d8b2577014a0f4d9696ac758c9f
nghugo <nghugoryan@gmail.com>
2024-06-27 22:42:53 +0100

    add styling to quicklinks

--- src/app/rlistData.js
+++ src/app/rlistData.js
@@ -24,6 +24,7 @@ import { fontSize } from "src/styles/fontConfig";
 export default function Rearrangablelist() {
   const screenWidth = Dimensions.get("window").width;
   const [contentData, setContentData] = useState([]);
+  const navigation = useNavigation();
 
   const router = useRouter();
   const db = useSQLiteContext();
@@ -47,28 +48,37 @@ export default function Rearrangablelist() {
           style={{
             minWidth: 100,
             minHeight: 100,
-            marginTop: 10,
             marginLeft: 10,
             marginRight: 10,
           }}
         >
           <TouchableOpacity
-            onLongPress={drag}
+            onLongPress={() => {
+              console.log("long press");
+              drag();
+            }}
+            onPress={() => {
+              console.log("short press");
+              navigation.navigate(`topics/[id]`, {
+                content: item.content,
+                title: item.title,
+              });
+              // pass parameters to pages https://reactnavigation.org/docs/params/#what-should-be-in-params
+            }}
             disabled={isActive}
             style={[
               styles.rowItem,
-              { backgroundColor: isActive ? "red" : item.backgroundColor },
+              { backgroundColor: isActive ? "red" : item.backgroundColor},
             ]}
           >
             <View style={{ width: "100%" }}>
-              <NavBlock
+              <TopicBlock
                 imageSource={require("assets/images/nhs-logo-square.png")}
                 title={item.title}
-                route={`/topics/${item.key}`}
+                route={`/topics/${item.key}`} // not really used, since passing content as prop already
                 description={item.description}
                 content={item.content}
               />
-              <Text>{item.title}</Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -113,9 +123,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   rowItem: {
-    height: 100,
     width: "100%",
-    // borderRadius: 20,
+    borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -137,64 +146,64 @@ const styles = StyleSheet.create({
   },
 });
 
-function NavBlock({ title, description, imageSource, content }) {
-  const navigation = useNavigation();
+function TopicBlock({ title, description, imageSource, content }) {
+  // const navigation = useNavigation();
 
-  const goToTopics = (content) => {
-    navigation.navigate(`topics/[id]`, { content: content, title: title });
-    // pass parameters to pages https://reactnavigation.org/docs/params/#what-should-be-in-params
-  };
+  // const goToTopics = (content) => {
+  //   navigation.navigate(`topics/[id]`, { content: content, title: title });
+  //   // pass parameters to pages https://reactnavigation.org/docs/params/#what-should-be-in-params
+  // };
 
   return (
-    <TouchableOpacity
-      onPress={() => {
-        goToTopics(content);
+    // <TouchableOpacity
+    //   onPress={() => {
+    //     goToTopics(content);
+    //   }}
+    // >
+    <View
+      style={{
+        flexDirection: "row",
+        gap: 10,
+        borderWidth: 1,
+        borderColor: "grey",
+        borderRadius: 20,
+        padding: 10,
       }}
     >
       <View
         style={{
-          flexDirection: "row",
-          gap: 10,
-          borderWidth: 1,
-          borderColor: "grey",
-          borderRadius: 20,
-          padding: 10,
+          flexDirection: "column",
+          justifyContent: "center",
         }}
       >
-        <View
+        <Image
+          resizeMethod="contain"
+          source={imageSource}
+          style={styles.nhsSquare}
+        />
+      </View>
+
+      <View style={{ flex: 1 }}>
+        <TText
           style={{
-            flexDirection: "column",
-            justifyContent: "center",
+            fontSize: fontSize.MEDIUM,
+            fontFamily: "InterMedium",
           }}
         >
-          <Image
-            resizeMethod="contain"
-            source={imageSource}
-            style={styles.nhsSquare}
-          />
-        </View>
-
-        <View style={{ flex: 1 }}>
-          <TText
-            style={{
-              fontSize: fontSize.MEDIUM,
-              fontFamily: "InterMedium",
-            }}
-          >
-            {title}
-          </TText>
-          <TText
-            style={{
-              fontSize: fontSize.SMALL,
-              fontFamily: "InterRegular",
-            }}
-            // numberOfLines="2"  // uncomment to abridge text
-            // ellipsizeMode="tail"  // uncomment to abridge text
-          >
-            {description}
-          </TText>
-        </View>
+          {title}
+        </TText>
+        <TText
+          style={{
+            fontSize: fontSize.SMALL,
+            fontFamily: "InterRegular",
+          }}
+          // numberOfLines="2"  // uncomment to abridge text
+          // ellipsizeMode="tail"  // uncomment to abridge text
+        >
+          {description}
+        </TText>
       </View>
-    </TouchableOpacity>
+    </View>
+    // </TouchableOpacity>
   );
 }

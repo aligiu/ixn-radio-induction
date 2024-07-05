6ac174906ef77870e63da9e69da1601e04862d42
nghugo <nghugoryan@gmail.com>
2024-07-06 00:38:28 +0100

    add file and secret rendering

--- src/components/FileModal.js
+++ src/components/FileModal.js
@@ -1,9 +1,17 @@
 import * as React from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
-import { Modal, Portal, Text, IconButton } from "react-native-paper";
+import { Modal, Portal, Text, IconButton, Icon } from "react-native-paper";
 import { TText } from "../app/_layout";
 
 import { fontSize } from "src/styles/fontConfig";
+import { TouchableOpacity } from "react-native";
+
+const fileDAO = [
+  { title: "procedures.docx", fileId: "123" },
+  { title: "details.pdf", fileId: "222" },
+  { title: "timetable1.pdf", fileId: "331" },
+  { title: "timetable2.pdf", fileId: "436" },
+];
 
 const FileModal = ({ visible, closeModal }) => {
   const containerStyle = {
@@ -48,18 +56,55 @@ const FileModal = ({ visible, closeModal }) => {
           </View>
         </View>
         <ScrollView style={{ paddingRight: 10 }}>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
+          {fileDAO &&
+            fileDAO.map((file, index) => (
+              <FileDownloadButton
+                key={index}
+                title={file.title}
+                fileId={file.fileId}
+              />
+            ))}
+          {fileDAO && fileDAO.length === 0 && (
+            <View>
+              <TText
+                style={{
+                  fontSize: fontSize.SMALL,
+                  fontFamily: "InterRegular",
+                }}
+              >
+                No files found
+              </TText>
+            </View>
+          )}
         </ScrollView>
       </Modal>
     </Portal>
+  );
+};
+
+const FileDownloadButton = ({ title, fileId }) => {
+  return (
+    <TouchableOpacity>
+      {/* fileId used as download link later */}
+      <View
+        flexDirection="row"
+        gap={10}
+        alignItems="center"
+        style={{ marginTop: 5, marginBottom: 5 }}
+      >
+        <Icon source="download" size={26} />
+        <TText
+          style={{
+            fontSize: fontSize.SMALL,
+            fontFamily: "InterRegular",
+            textDecorationLine: "underline",
+          }}
+        >
+          {" "}
+          {title !== "" ? title : "unnamed file"}{" "}
+        </TText>
+      </View>
+    </TouchableOpacity>
   );
 };
 
--- src/components/SecretModal.js
+++ src/components/SecretModal.js
@@ -48,15 +48,14 @@ const SecretModal = ({ visible, closeModal }) => {
           </View>
         </View>
         <ScrollView style={{ paddingRight: 10 }}>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
+          {/* If secrets found */}
+          <View>
+            <Text> Username: user1</Text>
+            <Text> Password: password1</Text>
+            <Text> Username: user2</Text>
+            <Text> Password: password2</Text>
+          </View>
+
         </ScrollView>
       </Modal>
     </Portal>

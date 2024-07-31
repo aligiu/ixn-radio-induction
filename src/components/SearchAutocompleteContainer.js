6b5f980f32bd917fbed4f16f6852570d104b4474
nghugo <nghugoryan@gmail.com>
2024-08-01 00:07:42 +0100

    bugfix lunr search

--- src/components/SearchAutocompleteContainer.js
+++ src/components/SearchAutocompleteContainer.js
@@ -11,7 +11,9 @@ import { TText } from "../app/_layout";
 import { fontSize } from "src/styles/fontConfig";
 
 function stripHtmlTags(html) {
-  return html.replace(/<\/?[^>]+(>|$)/g, "");
+  const noTags = html.replace(/<\/?[^>]+(>|$)/g, " ");
+  const noTagsAndTrim = noTags.replace(/ +/g, " ").trim(); // convert multiple spaces to one space and trim start and ends
+  return noTagsAndTrim;
 }
 
 // Function to add tagFreeContent field to each item in contentData
@@ -27,70 +29,74 @@ const SearchAutocompleteContainer = ({
   searchbarText,
   setSearchbarInFocus,
 }) => {
-  // Prepare data for Lunr.js
-  const documents = addTagFreeContent(contentData);
+  const query = searchbarText;
+  // const contentDataSearchRanked = search(query);
 
-  // Create Lunr.js index
-  const index = lunr(function () {
+  // console.log("query ->", query);
+  // console.log("contentDataSearchRanked ->", String(contentDataSearchRanked));
+
+  // Prepare data for Lunr.js
+  const docs = addTagFreeContent(contentData);
+
+  // console.log("docs:", docs.map((c) => {c.tagFreeContent}))
+  console.log(docs);
+
+  // Create Lunr index
+  const idx = lunr(function () {
+    this.ref("id"); // Document unique identifier
+    this.field("title");
     this.field("description");
     this.field("tagFreeContent");
     this.field("secret");
-    this.field("title");
-    this.ref("id");
 
-    documents.forEach((doc) => this.add(doc));
-  });
-
-  // Function to search and rank contentData
-  function search(query) {
-    const results = index.search(query);
-    console.log("searchResults", results);
-    return results.map((result) => {
-      const item = documents.find((doc) => doc.id === result.ref);
-      return { item, score: result.score };
-    });
-  }
-
-  const query = searchbarText;
-  const contentDataSearchRanked = search(query);
-
-  console.log("query ->", query);
-  console.log("contentDataSearchRanked ->", String(contentDataSearchRanked));
-
-  const keyboardHeight = useKeyboardHeight();
-
-  var fakeDocs = [
-    {
-      name: "Lunr",
-      text: "Like Solr, but much smaller, and not as bright.",
-    },
-    {
-      name: "React",
-      text: "A JavaScript library for building user interfaces.",
-    },
-    {
-      name: "Lodash",
-      text: "A modern JavaScript utility library delivering modularity, performance & extras.",
-    },
-  ];
-  // Create Lunr index
-  const idx = lunr(function () {
-    this.ref("name"); // Document unique identifier
-    this.field("text"); // Index and search within the 'text' field
-
-    fakeDocs.forEach((doc) => {
+    docs.forEach((doc) => {
       this.add(doc);
     });
   });
 
   // Perform a search
-  const results = idx.search("javascript");
+  const results = idx.search(query);
 
   // Log the results
   console.log("Search results:", results);
   results.forEach((result) => {
-    console.log(result.matchData.metadata)
+    console.log(result.matchData.metadata);
   });
+
+  const keyboardHeight = useKeyboardHeight();
+
+  // var fakeDocs = [
+  //   {
+  //     name: "Lunr",
+  //     text: "Like Solr, but much smaller, and not as bright.",
+  //   },
+  //   {
+  //     name: "React",
+  //     text: "A JavaScript library for building user interfaces.",
+  //   },
+  //   {
+  //     name: "Lodash",
+  //     text: "A modern JavaScript utility library delivering modularity, performance & extras.",
+  //   },
+  // ];
+  // // Create Lunr index
+  // const idx = lunr(function () {
+  //   this.ref("name"); // Document unique identifier
+  //   this.field("text"); // Index and search within the 'text' field
+
+  //   fakeDocs.forEach((doc) => {
+  //     this.add(doc);
+  //   });
+  // });
+
+  // // Perform a search
+  // const results = idx.search("javascript");
+
+  // // Log the results
+  // console.log("Search results:", results);
+  // results.forEach((result) => {
+  //   console.log(result.matchData.metadata)
+  // });
 
   return (
     <KeyboardAvoidingView style={{ flex: 1, marginBottom: keyboardHeight }}>
